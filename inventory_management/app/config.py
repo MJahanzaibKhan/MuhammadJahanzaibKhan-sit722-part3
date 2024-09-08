c20674bed03e2ac41130648f0c3d16cf6c2b3b65
MJahanzaibKhan <programmerjahanzaib7@gmail.com>
2024-09-08 17:10:32 +1000

    update files

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit722_part3_0bhs_user:hepsU6XQPsvMJF98zAnHYkfd5J1311sE@dpg-crekq65svqrc73fk5qf0-a.oregon-postgres.render.com/sit722_part3_0bhs')
